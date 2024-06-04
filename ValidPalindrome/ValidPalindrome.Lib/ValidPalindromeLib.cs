16e13198fe2022168224e7663ea644e41a7474a2
farooq-teqniqly <farooq@teqniqly.com>
2024-06-04 16:50:21 -0700

    ValidPalindrome

---
+++ ValidPalindrome/ValidPalindrome.Lib/ValidPalindromeLib.cs
@@ -0,0 +1,56 @@
+﻿namespace ValidPalindrome.Lib;
+
+public class ValidPalindromeLib
+{
+    private static readonly int MaxInputLength = (int)Math.Pow(10, 5) * 2;
+    public static bool Run(string input)
+    {
+        if (input.Length > MaxInputLength)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(input),
+                "Maximum length of the input is 2 * 10^5 characters.");
+        }
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return true;
+        }
+
+        var frontIndex = 0;
+        var backIndex = input.Length - 1;
+
+        while (frontIndex <= backIndex)
+        {
+            while (SkipCharacter(input, frontIndex))
+            {
+                frontIndex++;
+            }
+
+            while (SkipCharacter(input, backIndex))
+            {
+                backIndex--;
+            }
+
+            if (!CharactersAreEqual(input, frontIndex, backIndex))
+            {
+                return false;
+            }
+
+            frontIndex++;
+            backIndex--;
+        }
+
+        return true;
+    }
+
+    private static bool CharactersAreEqual(string input, int frontIndex, int backIndex)
+    {
+        return char.ToLowerInvariant(input[frontIndex]) == char.ToLowerInvariant(input[backIndex]);
+    }
+
+    private static bool SkipCharacter(string input, int index)
+    {
+        return !char.IsLetterOrDigit(input[index]);
+    }
+}
---
+++ ValidPalindrome/ValidPalindrome.Tests/ValidPalindromeTests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using ValidPalindrome.Lib;
+
+namespace ValidPalindrome.Tests;
+
+public class ValidPalindromeTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("    ")]
+    public void Empty_String_Is_A_Valid_Palindrome(string input)
+    {
+        ValidPalindromeLib.Run(input).Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("A man, a plan, a canal: Panama", true)]
+    [InlineData("abbba", true)]
+    [InlineData("race a car", false)]
+    public void ValidPalindrome_Returns_Correct_Result(string input, bool expectedResult)
+    {
+        ValidPalindromeLib.Run(input).Should().Be(expectedResult);
+    }
+
+    [Fact]
+    public void ValidPalindrome_Throws_When_Input_Is_Too_Long()
+    {
+        var length = (int)(2 * Math.Pow(10, 5)) + 1;
+        var input = new string('a', length);
+
+        var badAct = () => ValidPalindromeLib.Run(input);
+
+        badAct.Should().Throw<ArgumentOutOfRangeException>()
+            .WithMessage("Maximum length of the input is 2 * 10^5 characters. (Parameter 'input')");
+    }
+}

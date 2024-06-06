f0c2516e136baf3a3c7a5c657d746b759756f74d
farooq-teqniqly <farooq@teqniqly.com>
2024-06-05 18:09:30 -0700

    ValidParenthesis

---
+++ ValidParentheses/ValidParentheses.Lib/ValidParenthesesLib.cs
@@ -0,0 +1,55 @@
+﻿namespace ValidParentheses.Lib;
+
+public class ValidParenthesesLib
+{
+    public static bool Run(string input)
+    {
+        if (input.Length == 1)
+        {
+            return false;
+        }
+
+        var stack = new Stack<char>();
+
+        foreach (var ch in input)
+        {
+            if (IsOpenParenthesisCharacter(ch))
+            {
+                stack.Push(ch);
+            }
+            else if (IsClosedParenthesisCharacter(ch))
+            {
+                var poppedCh = stack.Pop();
+
+                if (!IsMatchingParenthesis(ch, poppedCh))
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.Count == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool IsMatchingParenthesis(char first, char second)
+    {
+        return (first == '(' && second == ')') ||
+               (first == '{' && second == '}') ||
+               (first == '[' && second == ']');
+    }
+
+    private static bool IsClosedParenthesisCharacter(char ch)
+    {
+        return ch == ')' || ch == ']' || ch == '}';
+    }
+
+    private static bool IsOpenParenthesisCharacter(char ch)
+    {
+        return ch == '(' || ch == '[' || ch == '{';
+    }
+}
---
+++ ValidParentheses/ValidParentheses.Tests/ValidParenthesesTests.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+using ValidParentheses.Lib;
+
+namespace ValidParentheses.Tests;
+
+public class ValidParenthesesTests
+{
+    [Theory]
+    [InlineData("(")]
+    [InlineData(")")]
+    [InlineData("[")]
+    [InlineData("]")]
+    [InlineData("{")]
+    [InlineData("}")]
+    public void ValidParentheses_When_Input_Is_Length_One_Returns_False(string input)
+    {
+        ValidParenthesesLib.Run(input).Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("(]", false)]
+    public void ValidParentheses_Returns_Correct_Result(string input, bool expectedResult)
+    {
+        ValidParenthesesLib.Run(input).Should().BeFalse();
+    }
+}

dc32aa6df573613f7eeef7a8345a376a993c65ff
farooq-teqniqly <farooq@teqniqly.com>
2024-06-06 15:26:49 -0700

    ReverseWords - bounds checking

--- ReverseWords/ReverseWords.Tests/ReverseWordsTests.cs
+++ ReverseWords/ReverseWords.Tests/ReverseWordsTests.cs
@@ -11,13 +11,48 @@ public class ReverseWordsTests
     [InlineData("the sky is blue", "blue is sky the")]
     [InlineData("  hello world  ", "world hello")]
     [InlineData("a good   example", "example good a")]
-    public void Reverse_Words_Returns_Expected_Result(string input, string reversed)
+    public void ReverseWords_Returns_Expected_Result(string input, string reversed)
     {
         foreach (var strategy in strategies)
         {
             var lib = new ReverseWordsLib(strategy);
 
             lib.Run(input).Should().Be(reversed);
+        }
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(null)]
+    public void ReverseWords_When_Input_Too_Short_Throws_Exception(string badInput)
+    {
+        foreach (var strategy in strategies)
+        {
+            var lib = new ReverseWordsLib(strategy);
+
+            var badAct = () => lib.Run(badInput);
+
+            badAct.Should()
+                .Throw<ArgumentException>()
+                .WithMessage("Input string cannot be null or whitespace only. (Parameter 'input')");
+        }
+    }
+
+    [Fact]
+    public void ReverseWords_When_Input_Too_Long_Throws_Exception()
+    {
+        var badInput = new string('a', (int)Math.Pow(10, 4) + 1);
+
+        foreach (var strategy in strategies)
+        {
+            var lib = new ReverseWordsLib(strategy);
+
+            var badAct = () => lib.Run(badInput);
+
+            badAct.Should()
+                .Throw<ArgumentException>()
+                .WithMessage("Maximum length of input string is 10^4 characters. (Parameter 'input')");
         }
     }
 }

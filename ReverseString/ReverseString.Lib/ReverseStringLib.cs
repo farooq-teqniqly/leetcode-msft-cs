b430856ac498f2cd34864d451641b99376edf642
farooq-teqniqly <farooq@teqniqly.com>
2024-06-06 14:45:55 -0700

    ReverseString

--- ReverseString/ReverseString.Lib/ReverseStringLib.cs
+++ ReverseString/ReverseString.Lib/ReverseStringLib.cs
@@ -2,8 +2,24 @@
 
 public class ReverseStringLib
 {
+    private const int MaxInputLength = 100_000;
+
     public static char[] Run(char[] input)
     {
+        if (input.Length == 0)
+        {
+            throw new ArgumentException(
+                "Input array cannot have a zero length.",
+                nameof(input));
+        }
+
+        if (input.Length > MaxInputLength)
+        {
+            throw new ArgumentException(
+                "The maximum length of the input array is 10^5 characters.",
+                nameof(input));
+        }
+
         var frontIndex = 0;
         var backIndex = input.Length - 1;
 
--- ReverseString/ReverseString.Tests/ReverseStringTests.cs
+++ ReverseString/ReverseString.Tests/ReverseStringTests.cs
@@ -8,11 +8,36 @@ public class ReverseStringTests
     [Theory]
     [InlineData("hello", "olleh")]
     [InlineData("Hannah", "hannaH")]
-    public void Reverse_String_Returns_Expected_Result(string input, string reversed)
+    [InlineData("a", "a")]
+    [InlineData("   a b a b  ", "  b a b a   ")]
+    public void ReverseString_Returns_Expected_Result(string input, string reversed)
     {
         var inputArray = input.ToCharArray();
         var reversedArray = reversed.ToCharArray();
 
         ReverseStringLib.Run(inputArray).Should().Equal(reversedArray);
+    }
+
+    [Fact]
+    public void ReverseString_When_Input_Is_Zero_Length_Throws_Exception()
+    {
+        var badAct = () => ReverseStringLib.Run(Array.Empty<char>());
+
+        badAct.Should()
+            .Throw<ArgumentException>()
+            .WithMessage("Input array cannot have a zero length. (Parameter 'input')");
+    }
+
+    [Fact]
+    public void ReverseString_When_Input_Is_Too_Long_Throws_Exception()
+    {
+        var badInput = Enumerable.Repeat('a', (int)Math.Pow(10, 5) + 1)
+            .ToArray();
+
+        var badAct = () => ReverseStringLib.Run(badInput);
+
+        badAct.Should()
+            .Throw<ArgumentException>()
+            .WithMessage("The maximum length of the input array is 10^5 characters. (Parameter 'input')");
     }
 }

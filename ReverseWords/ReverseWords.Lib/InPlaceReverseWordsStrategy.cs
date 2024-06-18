b3ce0f5e465f1e3f6c83570adc0cdbf9dda55c93
farooq-teqniqly <farooq@teqniqly.com>
2024-06-17 22:50:57 -0700

    Reverse words inplace

---
+++ ReverseWords/ReverseWords.Lib/InPlaceReverseWordsStrategy.cs
@@ -0,0 +1,48 @@
+﻿namespace ReverseWords.Lib;
+
+public class InPlaceReverseWordsStrategy : IReverseWordsStrategy
+{
+    public string Run(string input)
+    {
+        var inputArray = input.ToCharArray();
+
+        ReverseString(inputArray, 0, inputArray.Length - 1);
+        ReverseWords(inputArray);
+
+        return new string(inputArray);
+    }
+
+    private void ReverseWords(char[] inputArray)
+    {
+        var startWordIndex = 0;
+        var endWordIndex = 0;
+        var length = inputArray.Length;
+
+        while (startWordIndex < length)
+        {
+            while (endWordIndex < length && inputArray[endWordIndex] != ' ')
+            {
+                endWordIndex++;
+            }
+
+            ReverseString(inputArray, startWordIndex, endWordIndex - 1);
+            startWordIndex = endWordIndex + 1;
+            endWordIndex++;
+        }
+    }
+
+    private void ReverseString(char[] inputArray, int startIndex, int endIndex)
+    {
+        while (startIndex < endIndex)
+        {
+            char tmp = inputArray[endIndex];
+
+            inputArray[endIndex] = inputArray[startIndex];
+            inputArray[startIndex] = tmp;
+
+            startIndex++;
+            endIndex--;
+        }
+    }
+
+}

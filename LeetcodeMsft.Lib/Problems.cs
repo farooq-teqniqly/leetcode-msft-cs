1cced9db13f4bea4cc079c26f17394acb3744934
farooq-teqniqly <farooq@teqniqly.com>
2024-10-17 19:32:53 -0700

    Add Visual Studio solution, projects, and initial tests
    - Update .gitignore for Visual Studio-specific files and directories.
    - Add Visual Studio solution file `LeetcodeMsft.sln` with two projects.
    - Add `LeetcodeMsft.Lib.Tests.csproj` targeting .NET 8.0 with test dependencies.
    - Add initial unit test `UnitTest1` for `TwoSum` method using FluentAssertions.
    - Add `LeetcodeMsft.Lib.csproj` for the main library project targeting .NET 8.0.
    - Add `Problems.cs` with `TwoSum` method implementation.

---
+++ LeetcodeMsft.Lib/Problems.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetcodeMsft.Lib
+{
+    public class Problems
+    {
+        public static int[] TwoSum(int[] input, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var complement = target - input[i];
+
+                if (!map.ContainsKey(complement))
+                {
+                    map.Add(input[i], i);
+                }
+                else
+                {
+                    return new[] { map[complement], i };
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}

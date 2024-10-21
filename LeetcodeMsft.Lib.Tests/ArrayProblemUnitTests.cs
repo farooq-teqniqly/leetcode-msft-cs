407c440445c885764894df877157832e375d46f5
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-10-21 15:28:48 -0700

    Add RunningSum method and unit tests in C# project
    Introduce RunningSum method in ArrayProblems.cs to calculate the running sum of an array. Add unit tests in ArrayProblemUnitTests.cs using FluentAssertions for better readability. Ensure method handles null input by throwing ArgumentNullException.

---
+++ LeetcodeMsft.Lib.Tests/ArrayProblemUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace LeetcodeMsft.Lib.Tests
+{
+    public class ArrayProblemUnitTests
+    {
+        [Theory]
+        [InlineData(new[] { 2, 3, 5, 1, 6 }, new[] { 2, 5, 10, 11, 17 })]
+        [InlineData(new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3, 4, 5 })]
+        [InlineData(new[] { -1, 2, -3, 4, -5 }, new[] { -1, 1, -2, 2, -3 })]
+        [InlineData(new[] { 4 }, new[] { 4 })]
+        [InlineData(new[] { 4, -3 }, new[] { 4, 1 })]
+        [InlineData(new int[] { }, new int[] { })]
+        public void RunningSum_Returns_Expected_Result(int[] input, int[] expectedResult)
+        {
+            ArrayProblems.RunningSum(input).Should().Equal(expectedResult);
+        }
+
+        [Fact]
+        public void RunningSum_When_Input_Null_Throws()
+        {
+            var badAct = () => ArrayProblems.RunningSum(null!);
+            badAct.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
---
+++ LeetcodeMsft.Lib/ArrayProblems.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetcodeMsft.Lib
+{
+    public class ArrayProblems
+    {
+        /// <summary>
+        /// Calculates the running sum of an array.
+        /// </summary>
+        /// <param name="input">The input array.</param>
+        /// <returns>The running sum array.</returns>
+        public static int[] RunningSum(int[] input)
+        {
+            ArgumentNullException.ThrowIfNull(input);
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                input[i] += input[i - 1];
+            }
+
+            return input;
+        }
+    }
+}

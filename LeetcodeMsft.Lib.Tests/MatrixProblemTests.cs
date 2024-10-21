094d3468938c0c573a7b669226821a4c4ef29049
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-10-21 16:21:59 -0700

    Add RichestCustomer method and unit tests
    Implemented the `RichestCustomer` method in the `MatrixProblems` class within the `LeetcodeMsft.Lib` namespace. This method calculates the wealth of the richest customer based on their accounts and throws an `ArgumentNullException` if the input is null.
    
    Added unit tests for the `RichestCustomer` method in a new test class `MatrixProblemTests` using FluentAssertions for assertions. Introduced a new namespace `LeetcodeMsft.Lib.Tests` for the test class. Test cases include verifying correct functionality and ensuring an exception is thrown for null input.

---
+++ LeetcodeMsft.Lib.Tests/MatrixProblemTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+
+namespace LeetcodeMsft.Lib.Tests
+{
+    public class MatrixProblemTests
+    {
+        [Fact]
+        public void RichestCustomer_Returns_Expected_Result()
+        {
+            var testData = new List<Tuple<int[][], int>>
+            {
+                new(
+                [
+                    [5, 2, 3],
+                    [0, 6, 7]
+                ], 13),
+                new(
+                [
+                    [1, 2],
+                    [3, 4],
+                    [5, 6]
+                ], 11),
+                new(
+                [
+                    [10, 100],
+                    [200, 20],
+                    [30, 300]
+                ], 330),
+                new(
+                [
+                    [10, 100],
+                    [200, 20],
+                    [30]
+                ], 220),
+                new(
+                [
+                    [10, 100, 50, 100],
+                    [200, 20],
+                    [30]
+                ], 260)
+            };
+
+            foreach (var item in testData)
+            {
+                MatrixProblems.RichestCustomer(item.Item1).Should().Be(item.Item2);
+            }
+        }
+
+        [Fact]
+        public void RichestCustomer_When_Input_Null_Throws()
+        {
+            var badAct = () => MatrixProblems.RichestCustomer(null!);
+            badAct.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
---
+++ LeetcodeMsft.Lib/MatrixProblems.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetcodeMsft.Lib
+{
+    public class MatrixProblems
+    {
+        /// <summary>
+        /// Calculates the wealth of the richest customer based on their accounts.
+        /// </summary>
+        /// <param name="accounts">The array of customer accounts.</param>
+        /// <returns>The wealth of the richest customer.</returns>
+        public static int RichestCustomer(int[][] accounts)
+        {
+            ArgumentNullException.ThrowIfNull(accounts);
+
+            var customerCount = accounts.Length;
+            var maxWealth = int.MinValue;
+
+            for (var i = 0; i < customerCount; i++)
+            {
+                var wealth = 0;
+
+                for (var j = 0; j < accounts[i].Length; j++)
+                {
+                    wealth += accounts[i][j];
+                }
+
+                if (wealth > maxWealth)
+                {
+                    maxWealth = wealth;
+                }
+            }
+
+            return maxWealth;
+        }
+    }
+}

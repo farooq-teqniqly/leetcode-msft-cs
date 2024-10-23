62dcfbdf8082095b66a1e166683d39d125867ee0
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-10-22 22:12:05 -0700

    Add StackProblems class with ReverseString method and tests
    Introduce LeetcodeMsft.Lib.Tests namespace with StackProblems
    class and ReverseString method. This method reverses a string
    using a stack and includes null-checking to throw an
    ArgumentNullException for null inputs. Add StackProblemTests
    class using FluentAssertions to test various scenarios,
    including normal strings, empty strings, and null inputs.

---
+++ LeetcodeMsft.Lib.Tests/StackProblems.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetcodeMsft.Lib.Tests
+{
+    using System.Collections.Generic;
+
+    public class StackProblems
+    {
+        /// <summary>
+        /// Reverses a string.
+        /// </summary>
+        /// <param name="input">The input string to be reversed.</param>
+        /// <returns>The reversed string.</returns>
+        public static string ReverseString(string input)
+        {
+            ArgumentNullException.ThrowIfNull(input);
+
+            var stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            var output = new char[input.Length];
+            var index = 0;
+
+            while (stack.Count != 0)
+            {
+                output[index] = stack.Pop();
+                index++;
+            }
+
+            return new string(output);
+        }
+    }
+}

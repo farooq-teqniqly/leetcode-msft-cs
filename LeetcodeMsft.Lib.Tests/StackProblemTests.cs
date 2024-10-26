cfa64d242aa7d190abf11090ab4d2a3947e2fa50
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-10-26 12:45:37 -0700

    Add decimal to binary conversion with tests
    - Modified `using` directive for `FluentAssertions` to remove an invisible character.
    - Added namespace `LeetcodeMsft.Lib.Tests` to `StackProblemTests`.
    - Added `Can_Convert_Decimal_To_Binary` test method to `StackProblemTests`.
    - Added `DecimalToBinary_When_Input_Negative_Throws` test method to `StackProblemTests`.
    - Added `using System.Text;` directive to `StackProblems.cs`.
    - Implemented `DecimalToBinary` method in `StackProblems` class to convert decimal to binary and handle negative input.

--- LeetcodeMsft.Lib.Tests/StackProblemTests.cs
+++ LeetcodeMsft.Lib.Tests/StackProblemTests.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 
 namespace LeetcodeMsft.Lib.Tests;
 
@@ -19,5 +19,23 @@ public class StackProblemTests
     {
         var badAct = () => StackProblems.ReverseString(null!);
         badAct.Should().Throw<ArgumentNullException>();
+    }
+
+    [Theory]
+    [InlineData(0, "0")]
+    [InlineData(1, "1")]
+    [InlineData(2, "10")]
+    [InlineData(7, "111")]
+    [InlineData(18, "10010")]
+    public void Can_Convert_Decimal_To_Binary(int input, string expectedResult)
+    {
+        StackProblems.DecimalToBinary(input).Should().Be(expectedResult);
+    }
+
+    [Fact]
+    public void DecimalToBinary_When_Input_Negative_Throws()
+    {
+        var act = () => StackProblems.DecimalToBinary(-1);
+        act.Should().Throw<ArgumentOutOfRangeException>();
     }
 }
--- LeetcodeMsft.Lib/StackProblems.cs
+++ LeetcodeMsft.Lib/StackProblems.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetcodeMsft.Lib;
+using System.Text;
+
+namespace LeetcodeMsft.Lib;
 
 public class StackProblems
 {
@@ -28,5 +30,38 @@ public class StackProblems
         }
 
         return new string(output);
+    }
+    /// <summary>
+    /// Converts a decimal number to binary representation.
+    /// </summary>
+    /// <param name="input">The decimal number to convert.</param>
+    /// <returns>The binary representation of the decimal number.</returns>
+    public static string DecimalToBinary(int input)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(input, 0);
+
+        if (input == 0)
+        {
+            return "0";
+        }
+
+        var stack = new Stack<int>();
+
+        var divisor = input;
+
+        while (divisor > 0)
+        {
+            stack.Push((divisor % 2));
+            divisor /= 2;
+        }
+
+        var output = new StringBuilder(stack.Count);
+        
+        while (stack.Count != 0)
+        {
+            output.Append(stack.Pop());
+        }
+        
+        return output.ToString();
     }
 }
